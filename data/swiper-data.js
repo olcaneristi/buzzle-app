ca84416a20f8ae72828a3625b15991c7cb2efaa1
oğulcan erişti <ogulcaneristi123@gmail.com>
2021-12-27 01:11:42 +0300

    add swiper for intro

---
+++ data/swiper-data.js
@@ -0,0 +1,26 @@
+export const data = [
+   {
+      title: 'Enterprise team collaboration.',
+      text: 'Bring together your files, your tools, projects and people. Including a new mobile and desktop application.',
+      image: require('../assets/slide3.png'),
+      bg: '#59b2ab',
+   },
+   {
+      title: 'Enterprise team collaboration.',
+      text: "Lot's of cool libraries!",
+      image: require('../assets/slide3.png'),
+      bg: '#febe29',
+   },
+   {
+      title: 'Enterprise team collaboration.',
+      text: 'This is the first swipe component!',
+      image: require('../assets/slide3.png'),
+      bg: '#22bcb5',
+   },
+   {
+      title: 'Enterprise team collaboration.',
+      text: 'Joking!',
+      image: require('../assets/slide3.png'),
+      bg: '#febe29',
+   },
+];
