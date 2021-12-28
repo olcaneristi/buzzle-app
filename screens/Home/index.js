a8274614ec3757fdf5870aa88e9475930405e1ce
oğulcan erişti <ogulcaneristi123@gmail.com>
2021-12-28 20:32:20 +0300

    add refresh indicator for swipe

--- screens/Home/index.js
+++ screens/Home/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { styles } from '../../styles/home.styles';
+import React, { useState, useCallback } from 'react';
+import { ScrollView, SafeAreaView, RefreshControl } from 'react-native';
+import { Header, InstaStories, Posts } from '../../components';
+import { postList } from '../../data/home-data';
 
+const wait = timeout => {
+  return new Promise(resolve => setTimeout(resolve, timeout));
+};
 const Home = () => {
+  const [refreshTimeline, setRefreshTimeline] = useState(false);
+
+  const onRefresh = useCallback(() => {
+    setRefreshTimeline(true);
+    wait(2000).then(() => setRefreshTimeline(false));
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+      <Header />
+      <ScrollView refreshControl={<RefreshControl refreshing={refreshTimeline} onRefresh={onRefresh} />}>
+        <InstaStories />
+        {postList && postList.map(post => <Posts key={post.id} post={post} />)}
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
